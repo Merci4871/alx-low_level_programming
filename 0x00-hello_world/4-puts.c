5379f6c2f877d99ad77d1fbfa2a07bfcb1834b62
Merci4871 <habamungumercieric@gmail.com>
2024-01-02 11:58:51 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the entry fucton for execution
+ * Return: here it will return an integer value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
