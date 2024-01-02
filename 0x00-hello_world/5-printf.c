b1279111e0c26f7e81d8df8baf639f2b5b4fe3ad
Merci4871 <habamungumercieric@gmail.com>
2024-01-02 12:34:03 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point for c execution
+ *
+ * Return: it will return integer value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
