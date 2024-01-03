cdbd9e1a9d62afb3dd7facce89001f2d134509cd
Merci4871 <habamungumercieric@gmail.com>
2024-01-03 00:40:22 -0800

    task 6 co{

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - this will be the entery point of execution
- * Return: after successes it will return an integer value
- */
+* main - Entry point of the program
+* Return: 0 upon successful execution
+*/
 int main(void)
 {
-	char c;
-	long int l;
-	int x;
-	float f;
-	long long int y;
+char c;
+int x;
+long int l;
+long long int y;
+float f;
 
-	printf("size of a char : %lu byte(s)\t", sizeof(c));
-	printf("\n size of an int : %lu byte(s)\t", sizeof(x));
-	printf("\n size of a long int : %lu byte(s)\t", sizeof(l));
-	printf("\n size of a long long int : %lu byte(s)\t", sizeof(y));
-	printf("\n size of a float : %lu byte(s)\t", sizeof(f));
-	return (0);
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(x));
+printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(y));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
+return (0);
 }
