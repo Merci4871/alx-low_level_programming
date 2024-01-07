dc1072927842d53ddec5714ee11cc2fe608a676c
Merci4871 <habamungumercieric@gmail.com>
2024-01-07 07:00:00 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - the first entry point to execution
+* Return: always 0
+*/
+int main(void)
+{
+int x = 0;
+
+while (x <= 9)
+{
+printf("%d", x);
+x++;
+}
+printf("\n");
+return (0);
+}
