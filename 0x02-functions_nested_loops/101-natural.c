d8415a5f1299acff7a4bd4ac610757e38899fc9b
Merci4871 <habamungumercieric@gmail.com>
2024-01-27 02:04:44 -0800

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+/* 101-natural.c */
+#include <stdio.h>
+/**
+ * main - the frst enrty point
+ * Return: always zero
+ */
+
+int main(void)
+{
+int sum = 0;
+
+for (int i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
