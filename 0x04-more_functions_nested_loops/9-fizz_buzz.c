0d5797f82b7adab29f5cf5808c4b320b2f2c0bd1
Merci4871 <habamungumercieric@gmail.com>
2024-02-29 03:51:14 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - the first entry point to a program
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
